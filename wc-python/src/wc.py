9dca69ffcba2120285dec06f5e28e936657652f0
Nicolas Hahn <nicolas@stonespring.org>
2018-07-22 19:03:29 -0400

    linted

--- wc-python/src/wc.py
+++ wc-python/src/wc.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
+"""Emulates wc -l using concurrent processes/threads."""
 
 import sys
 import os
 from concurrent import futures
 
-def line_count(filename):
-    with open(filename, 'r') as f:
-        lines = f.readlines()
+
+def count_lines(filename):
+    """Open file, count newlines, then return (filename, line_count) tuple."""
+    with open(filename, 'r') as file_to_read:
+        lines = file_to_read.readlines()
         line_count = len(lines)
         # Because readlines() returns a line even if the only
         # text in the file is not terminated by a newline
@@ -15,11 +18,17 @@ def line_count(filename):
                 line_count = 0
         return filename, line_count
 
+
 def print_count(count, label):
+    """Print a line count right-justified along with a label."""
     formatted_count = str(count).rjust(10)
     print "{} {}".format(formatted_count, label)
 
+
 def main():
+    """Takes directory as argument, returns number of newlines in all files in
+    the that directory (non-recursively).
+    """
     if len(sys.argv) < 2:
         dirname = '.'
         paths = [f for f in os.listdir(dirname)]
@@ -30,14 +39,15 @@ def main():
     counts = {}
     # with futures.ThreadPoolExecutor(max_workers=len(filenames)) as executor:
     with futures.ProcessPoolExecutor(max_workers=len(filenames)) as executor:
-        all_futures = [executor.submit(line_count, f) for f in filenames]
+        all_futures = [executor.submit(count_lines, f) for f in filenames]
         for future in futures.as_completed(all_futures):
             filename, count = future.result()
             counts[filename] = count
-    ranked = sorted(counts, key=lambda k:-counts[k])
+    ranked = sorted(counts, key=lambda k: -counts[k])
     for filename in ranked:
         print_count(counts[filename], filename)
     print_count(sum(counts.values()), "[TOTAL]")
+
 
 if __name__ == "__main__":
     main()
